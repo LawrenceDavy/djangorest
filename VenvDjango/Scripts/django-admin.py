a1a81395f1d730cd6dce3f7c421819eb9ee52b6b
LawrenceDavy13 <LawrenceDavy13@gmail.com>
2019-07-01 21:33:41 +0100

    planning stage

---
+++ VenvDjango/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\davyl\documents\go\src\github.com\lawrencedavy13\djangorest\venvdjango\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
